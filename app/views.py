cdb8cd092db32fe59c6c6b5c2b5a9651c1f85cfc
roornelas <r.ornelas.v@outlook.com>
2015-09-25 15:36:43 -0400

    backend code working

--- app/views.py
+++ app/views.py
@@ -1,6 +1,24 @@
-from app import app
+from app import db, models, app
+from flask import request
 
 
 @app.route('/', methods=['GET'])
 def index():
     return 'Hello world!'
+
+@app.route('/health', methods=['GET'])
+def health():
+    return 'OK!', 200
+
+@app.route('/coordinates', methods=['POST'])
+def coordinates():
+    content = request.get_json(force=True)
+    coors = content['coordinates']
+    print coors
+    for coor in coors:
+        print coor
+        db.session.add(models.Coordinates(coor["latitude"], coor["longitude"], coor["notes"]))
+    db.session.commit()
+    return 'OK 12' , 200
+
+
