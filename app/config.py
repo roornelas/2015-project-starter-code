c083d06c0fbf2da5f5ec0db07c10c7ca5df4d03f
roornelas <r.ornelas.v@outlook.com>
2015-09-26 19:42:46 -0400

    fixed bug

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     WTF_CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db’)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
